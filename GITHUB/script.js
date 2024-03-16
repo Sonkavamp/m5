fe8dcb80c3048d6864bc949e6ce0706612880ed7
Sonkavamp <sofagrevceva0@gmail.com>
2024-03-16 18:03:48 +0200

    first

---
+++ GITHUB/script.js
@@ -0,0 +1,16 @@
+let searchValue = document.getElementById('searchValue')
+let searchBtn = document.getElementById('searchBtn')
+let card = document.getElementById('card')
+let load = document.getElementById('load')
+
+let dataCard;
+
+searchBtn.addEventListener("click", getcard);
+
+card.classList.add("d-none");
+load.classList.add("d-none");
+
+function getCard() {
+    card.classList.add("d-none");
+    load.classList.remove("d-none")
+}
